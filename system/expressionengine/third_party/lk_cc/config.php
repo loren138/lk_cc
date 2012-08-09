57b35376923a26e484b288b870ddb1e2ee729e0c
Loren <loren@klingman.us>
2012-08-09 15:14:03 +0700

    Read me

--- system/expressionengine/third_party/lk_cc/config.php
+++ system/expressionengine/third_party/lk_cc/config.php
@@ -4,7 +4,7 @@ if (! defined('LK_CC_VER'))
 	define('LK_CC_NAME', 'LK Constant Contact');
 	define('LK_CC_VER',  '1.0');
 	define('LK_CC_AUTHOR',  'Loren Klingman');
-	define('LK_CC_DOCS',  'http://devot-ee.com/add-ons/lk_cc');
+	define('LK_CC_DOCS',  'https://github.com/wiseloren/lk_cc');
 	define('LK_CC_DESC',  'LK Constant Contact: Interfaces with the Constant Contact API to sign users up for newsletters.');
 	
 }
